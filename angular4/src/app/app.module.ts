12bce68d48b73ca26cf1fede7f7cba4432a2e80d
hareshp-ezdi <haresh.p@ezdi.us>
2018-04-09 19:33:41 +0530

    initial command ng new myangular4

--- angular4/src/app/app.module.ts
+++ angular4/src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {ProductItemComponent} from './product-item/product-item.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {SearchComponent} from './search/search.component';
+import {ProductService} from './shared/product.service';
+import {Router, RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'products/:productId', component: ProductDetailComponent}
+    ]),
+    FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
